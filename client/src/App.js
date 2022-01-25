506bc07a16989b4da3090ee168434bdae538b4ac
Ibrahim Abulaila <abeabulaila@gmail.com>
2022-01-24 19:22:59 -0500

    succesfully connected front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import portal from './Images/p2.gif'
+import Welcome from './Components/Welcome';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="welcome">
+        <Welcome />
+
+      </div>
+      <div>
+        <img className='portal' src={portal} />
+
+      </div>
+      <div className='enter-button'>
+        <Button type="button" class="btn btn-secondary">Enter If You Dare!</Button>
+      </div>
     </div>
   );
 }
---
+++ client/src/Components/Welcome.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Welcome(){
+    
+    
+    
+    
+
+
+
+
+
+    return(
+        <div>
+            <h1>Welcome to Cascade</h1>
+        </div>
+    )
+}
+
+export default Welcome
