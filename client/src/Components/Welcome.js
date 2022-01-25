ec3630659340966942d6f0bb38d1df205c2a689e
Ibrahim Abulaila <abeabulaila@gmail.com>
2022-01-25 09:55:25 -0500

    added signup form in beginning

--- client/src/Components/Welcome.js
+++ client/src/Components/Welcome.js
@@ -14,6 +14,17 @@ function Welcome() {
             <div>
                 <img className='portal' src={portal} />
             </div>
+            <div>
+                <h3 className="acc-title">Make An Account</h3>
+                <form className="input-form">
+                    <label><strong>Username: </strong> 
+                        <input type='text' name='name' />
+                    </label>
+                    <label> <strong> Password: </strong>
+                        <input type='text' password='password'/>
+                    </label>
+                </form>
+            </div>
             <div className='enter-button'>
                 <button type="button" class="btn btn-dark btn-lg">Enter If You Dare</button>
             </div>
