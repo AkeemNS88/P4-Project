7bfedcc231e1c668a43cb72e16bb70a1f234934a
Ibrahim Abulaila <abeabulaila@gmail.com>
2022-01-25 12:38:29 -0500

    finished choose your fighter page, looks decent enough to continue forward

--- client/src/Components/ChooseFighter.js
+++ client/src/Components/ChooseFighter.js
@@ -1,28 +1,37 @@
 import React from "react";
 import enemy from "./Encounters/set1-1.jpg"
+import Button from 'react-bootstrap/Button';
 
 
 function ChooseFighter() {
     return (
-        <div>
+        <div id="wrapper">
             <div>
                 <h1 className="welcome">Choose Your Fighter!</h1>
             </div>
 
-            <div>
-                <h3 className="warrior">Warrior</h3>
-                <img className="choose" src={enemy} />
+            <h3 className="warrior">Warrior</h3>
+            <div className="class-descrip">
+                <h3 className="class-text"> Warrior, fierce and formidable. Starts with 150 health, 50 wealth, and 75 energy.</h3>
+                <Button variant="dark" className="choose-champ" style={{fontSize: "25px"}}>Choose Warrior</Button>
             </div>
-            <div>
+            <img className="choose" src={enemy} />
+
             <h3 className="mage">Mage</h3>
-
-                <img className="choose" src={enemy} />
+            <div className="class-descrip">
+                <h3 className="class-text" > Mage, wise and cautious, in tune with the spiritual nature of the world. Starts with 100 health, 50 wealth, and 200 energy.</h3>
+                <Button variant="dark" className="choose-champ" style={{fontSize: "25px"}}>Choose Mage</Button>
             </div>
-            <div>
+            <img className="choose" src={enemy} />
+
+
             <h3 className="ranger">Ranger</h3>
-
-                <img className="choose" src={enemy} />
+            <div className="class-descrip">
+                <h3 className="class-text"> Ranger, sharp shot, great attention to detail. Finds what most others don't. Starts with 150 health, 50 wealth, and 75 energy.</h3>
+                <Button variant="dark" className="choose-champ" style={{fontSize: "25px"}}>Choose Ranger</Button>
             </div>
+            <img className="choose" src={enemy} />
+
         </div>
     )
 }
