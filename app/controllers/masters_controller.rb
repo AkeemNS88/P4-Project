a68929f2e9b8a6f3d143c0acecd085b0d5fdd8ab
Akeem <AkeemS11788@gmail.com>
2022-01-25 19:19:50 -0500

    index/show test

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -1,2 +1,18 @@
-class MasterssController < ApplicationController
+class MastersController < ApplicationController
+
+    def index
+        render json: Master.all
+    end
+
+    def show
+        master = find_master
+        render json: master
+    end
+
+
+    private
+
+    def find_master
+        Master.find(params[:id])
+    end
 end
